8d455fd4e303a15bdef6cfd63ab3cc1475ab5e0d
Aaron Patterson <aaron.patterson@gmail.com>
2009-10-18 23:31:50 -0700

    inital add

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  MeetingTime2
+//
+//  Created by Aaron Patterson on 10/18/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MeetingTime2
+//
+//  Created by Aaron Patterson on 10/18/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
